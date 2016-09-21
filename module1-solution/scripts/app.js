89e234938e34476a0a7d4eeec0fedaaecb53a676
fl315ch3r <fabian.delmonte@gmail.com>
2016-09-20 23:44:43 -0300

    implementing angularjs

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -6,6 +6,22 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
-    
+
+    $scope.message = "";
+
+    $scope.checkIfTooMuch = function() {
+      var menu = [];
+      if ($scope.lunchMenu == ""){
+        $scope.message = "Please enter data first";
+      }else {
+        menu = $scope.split(',');
+        if (menu.length > 0 && menu.length <= 3) {
+          $scope.message = "Enjoy!";
+        } else if (menu.length > 3) {
+          $scope.message = "Too much!";
+        }
+      }
+    }
+
   }
 })();
